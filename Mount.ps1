3856babb3d6d7d8cab494c6e3c702cc1cf93416b
Alex Bowen <106083652+alex-mercia-solutions@users.noreply.github.com>
2022-05-27 10:31:47 +0100

    Add files via upload

---
+++ Mount.ps1
@@ -0,0 +1,29 @@
+﻿#Mercia Solutions mount iso images version 0.1
+
+#setting to unrestrict ---  Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+#Default setting  --- Set-ExecutionPolicy -ExecutionPolicy restricted
+
+Foreach($file in Get-ChildItem "C:\ISO_IMAGES" -Filter *.iso)
+ {    
+   $imagename = $file.ToString()
+
+   #create output folder variable
+   $folder = Join-Path -Path 'c:\Jukebox' -ChildPath $imagename.Substring(0,$imagename.Length-4)
+   Write-Host $folder
+  
+   #create full path to iso image
+   $fullimagepath = Join-Path -Path 'C:\ISO_IMAGES' -ChildPath $imagename
+   Write-Host $fullimagepath
+
+   #create the directory to mount
+   New-Item -Path "c:\jukebox" -Name $imagename.Substring(0,$imagename.Length-4) -ItemType "directory"
+   
+   # Mount the image without a drive letter
+   $ImageID = Mount-DiskImage -ImagePath $fullimagepath  -NoDriveLetter
+
+   # Get mounted ISO volume
+   $volInfo = $ImageID | Get-Volume
+
+   # Mount volume with specified drive letter (requires Administrator access)
+   mountvol $folder $volInfo.UniqueId
+}
---
+++ unMount.ps1
@@ -0,0 +1,18 @@
+﻿#Mercia Solutions unmount iso images version 0.1
+
+Foreach($file in Get-ChildItem "C:\ISO_IMAGES" -Filter *.iso)
+{
+    $imagename = $file.ToString() 
+     
+    $fullimagepath = Join-Path -Path 'C:\ISO_IMAGES' -ChildPath $imagename
+    Write-Host $fullimagepath
+
+    #unmount the image
+    DisMount-DiskImage -ImagePath $fullimagepath
+      
+    $folder = Join-Path -Path 'c:\Jukebox' -ChildPath $imagename.Substring(0,$imagename.Length-4)
+    Write-Host $folder
+        
+    #remove the folder
+    Remove-Item $folder -Force
+}
